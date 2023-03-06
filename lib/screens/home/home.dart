5f98c172395d25190f2b56c554d0b8d7d66de474
Mustafa <mmermmus123@gmail.com>
2023-03-06 20:45:59 +0300

    home screen's parts

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,7 @@
+import 'package:art_exhibition/constants/constant_colors.dart';
+import 'package:art_exhibition/utilities/extension_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -8,11 +11,87 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 13, 13, 13),
       appBar: AppBar(),
-      body: Center(
-        child: Text(
-          "home",
-          style: Theme.of(context).textTheme.displayLarge,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: context.lowRateWidth * 2),
+        child: Column(
+          children: [
+            _tittle(context),
+            _slider(),
+            _categories(),
+            _bottomNavigationBar(),
+          ],
         ),
+      ),
+    );
+  }
+
+  Row _tittle(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(bottom: context.lowRateWidth),
+          child: Text(
+            "DailyContent",
+            style: GoogleFonts.oxygen(
+              textStyle: TextStyle(
+                color: Colors.white,
+                fontSize: context.dynamicWidth(0.08),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Expanded _slider() {
+    return Expanded(
+      flex: 70,
+      child: Container(
+        color: ConstantColors.colorLoginButton.getColor,
+      ),
+    );
+  }
+
+  Expanded _categories() {
+    return Expanded(
+      flex: 20,
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                color: ConstantColors.colorTextfield.getColor,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                color: ConstantColors.colorTextfield.getColor,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                color: ConstantColors.colorTextfield.getColor,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Expanded _bottomNavigationBar() {
+    return Expanded(
+      flex: 15,
+      child: Container(
+        color: Colors.red,
       ),
     );
   }
