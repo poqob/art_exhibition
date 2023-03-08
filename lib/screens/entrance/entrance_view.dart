9acb83da86a1880c2531f365ea6272ded8d1268a
Mustafa <mmermmus123@gmail.com>
2023-03-08 03:39:21 +0300

    skip button, page indicator added. Login page binded.

--- lib/screens/entrance/entrance_view.dart
+++ lib/screens/entrance/entrance_view.dart
@@ -1,9 +1,12 @@
 import 'package:art_exhibition/constants/constant_colors.dart';
+import 'package:art_exhibition/screens/authentaticion/authentaticion_login_view.dart';
 import 'package:art_exhibition/screens/entrance/entrance_route.dart';
+import 'package:art_exhibition/utilities/extension_layout.dart';
 import 'package:art_exhibition/widgets/entrance/next.dart';
 import 'package:art_exhibition/widgets/entrance/sub.dart';
 import 'package:art_exhibition/widgets/entrance/top.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 //responsive entrance page.
 class EntranceScreen extends StatefulWidget {
@@ -14,25 +17,85 @@ class EntranceScreen extends StatefulWidget {
 }
 
 class _EntranceScreenState extends State<EntranceScreen> {
+  final PageController _controller = PageController();
+  final int msDuration = 600;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            flex: 65,
-            child: top(ConstantColors.colorEntranceTheme.getColor,
-                entranceInstance.pageContent.imagePath),
+          PageView(
+            controller: _controller,
+            pageSnapping: true,
+            allowImplicitScrolling: true,
+            scrollDirection: Axis.horizontal,
+            children: [
+              _page(context, EntrancePages.entrance0),
+              _page(context, EntrancePages.entrance1),
+              _page(context, EntrancePages.entrance2),
+              const LogIn(),
+            ],
           ),
-          Expanded(
-            flex: 35,
-            child: sub(context, entranceInstance.pageContent.heading,
-                entranceInstance.pageContent.content),
-          )
+          !isLastPage
+              ? Positioned(
+                  top: 15,
+                  right: 15,
+                  child: TextButton(
+                    child: Text(
+                      "skip",
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    onPressed: () {
+                      _controller.animateToPage(2,
+                          duration: Duration(milliseconds: msDuration),
+                          curve: Curves.linear);
+
+                      setState(() {
+                        entranceInstance = EntrancePages.entrance2;
+                      });
+                    },
+                  ),
+                )
+              : const SizedBox(),
+          !isLastPage
+              ? Positioned(
+                  top: context.dynamicHeight(0.6),
+                  right: 10,
+                  child: SmoothPageIndicator(
+                    controller: _controller, // PageController
+                    count: EntrancePages.values.length,
+                    effect: const WormEffect(), // your preferred effect
+                    onDotClicked: (index) {},
+                  ),
+                )
+              : const SizedBox(),
         ],
       ),
-      floatingActionButton:
-          nextButton(ConstantColors.colorEntranceTheme.getColor, context, this),
+      floatingActionButton: !isLastPage
+          ? nextButton(
+              context,
+              this,
+              _controller,
+              msDuration,
+            )
+          : const SizedBox(),
+    );
+  }
+
+  Column _page(BuildContext context, EntrancePages content) {
+    return Column(
+      children: [
+        Expanded(
+          flex: 65,
+          child: top(ConstantColors.colorEntranceTheme.getColor,
+              content.pageContent.imagePath),
+        ),
+        Expanded(
+          flex: 35,
+          child: sub(context, content.pageContent.heading,
+              content.pageContent.content),
+        )
+      ],
     );
   }
 }
