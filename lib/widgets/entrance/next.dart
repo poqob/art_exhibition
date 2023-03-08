dd728a739bf363483ae149715c462b914962f9e8
Mustafa <mmermmus123@gmail.com>
2023-03-08 03:38:39 +0300

    code cleaned

--- lib/widgets/entrance/next.dart
+++ lib/widgets/entrance/next.dart
@@ -1,29 +1,43 @@
+import 'package:art_exhibition/constants/constant_colors.dart';
 import 'package:art_exhibition/screens/entrance/entrance_route.dart';
 import 'package:flutter/material.dart';
 
-FloatingActionButton nextButton(
-    Color color, BuildContext context, State state) {
+FloatingActionButton nextButton(BuildContext context, State state,
+    PageController controller, int msDuration) {
   return FloatingActionButton(
     onPressed: () {
-      state.setState(() {
-        switch (entranceInstance) {
-          case EntrancePages.entrance0:
-            entranceInstance = EntrancePages.entrance1;
-            break;
-          case EntrancePages.entrance1:
-            entranceInstance = EntrancePages.entrance2;
-            break;
-          case EntrancePages.entrance2:
-            //animated push
-            Navigator.popAndPushNamed(context, "/login");
-            break;
-          default:
+      state.setState(
+        () {
+          switch (entranceInstance) {
+            case EntrancePages.entrance0:
+              controller.animateToPage(1,
+                  duration: Duration(milliseconds: msDuration),
+                  curve: Curves.linear);
+              entranceInstance = EntrancePages.entrance1;
 
-          //route to logIn screen
-        }
-      });
+              break;
+            case EntrancePages.entrance1:
+              controller.animateToPage(2,
+                  duration: Duration(milliseconds: msDuration),
+                  curve: Curves.linear);
+              entranceInstance = EntrancePages.entrance2;
+              break;
+            case EntrancePages.entrance2:
+              //animated push
+              isLastPage = true;
+              controller.animateToPage(3,
+                  duration: Duration(milliseconds: msDuration),
+                  curve: Curves.linear);
+              //Navigator.popAndPushNamed(context, "/login");
+              break;
+            default:
+
+            //route to logIn screen
+          }
+        },
+      );
     },
-    backgroundColor: color,
+    backgroundColor: ConstantColors.colorEntranceTheme.getColor,
     elevation: 20,
     tooltip: "next",
     child: Center(
--- lib/widgets/entrance/top.dart
+++ lib/widgets/entrance/top.dart
@@ -1,31 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-Container top(Color color, String path) {
-  return Container(
-    decoration: BoxDecoration(
-      color: color,
-      borderRadius: const BorderRadius.only(
-        bottomLeft: Radius.circular(90.0),
-        bottomRight: Radius.circular(225.0),
-      ),
-    ),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        const Spacer(),
-        Expanded(
-          flex: 6,
-          child: Center(
-            child: SvgPicture.asset(
-              path,
-              alignment: Alignment.topCenter,
-              semanticsLabel: "entrance0",
-            ),
+Stack top(Color color, String path) {
+  return Stack(
+    children: [
+      Container(
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(90.0),
+            bottomRight: Radius.circular(225.0),
           ),
         ),
-        const Spacer(),
-      ],
-    ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Spacer(),
+            Expanded(
+              flex: 6,
+              child: Center(
+                child: SvgPicture.asset(
+                  path,
+                  alignment: Alignment.topCenter,
+                  semanticsLabel: "entrance0",
+                ),
+              ),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    ],
   );
 }
