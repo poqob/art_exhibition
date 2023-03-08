95b96baa3cb440c5850b8dec19b89649093699a2
Mustafa <mmermmus123@gmail.com>
2023-03-08 21:46:30 +0300

    custom glass effected box widget writed.

---
+++ lib/widgets/common/glassBox.dart
@@ -0,0 +1,60 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+Widget glassBox({
+  required BuildContext context,
+  BorderRadiusGeometry border = BorderRadius.zero,
+  double borderOpacity = 0.2,
+  required double width,
+  required double height,
+  Widget? child,
+  Alignment alignment = Alignment.center,
+  double blur = 1.0,
+}) {
+  return ClipRRect(
+    clipBehavior: Clip.antiAlias,
+    borderRadius: border,
+    child: Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        borderRadius: border,
+      ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          //filter
+
+          BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: blur,
+              sigmaY: blur,
+            ),
+            child: Container(),
+          ),
+
+          //gradiant effect
+          Container(
+            alignment: alignment,
+            width: width,
+            height: height,
+            decoration: BoxDecoration(
+              border:
+                  Border.all(color: Colors.white.withOpacity(borderOpacity)),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Colors.white.withOpacity(0.4),
+                  Colors.white.withOpacity(0.1)
+                ],
+              ),
+              borderRadius: border,
+            ),
+            child: child,
+          ),
+        ],
+      ),
+    ),
+  );
+}
