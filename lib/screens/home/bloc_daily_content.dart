cad23c919bb3f34b2f8fe7b4376c656d711760e6
Mustafa <mmermmus123@gmail.com>
2023-03-09 20:27:39 +0300

    cubit structure codded to obtain data from db

---
+++ lib/screens/home/bloc_daily_content.dart
@@ -0,0 +1,38 @@
+import 'package:art_exhibition/db/api/db.dart';
+import 'package:art_exhibition/db/api/queries.dart';
+import 'package:art_exhibition/db/models/art/model_art.dart';
+import 'package:art_exhibition/screens/home/states_daily_content.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class DailyContentCubit extends Cubit<DailyContentStates> {
+  DailyContentCubit() : super(const DailyInitial());
+  //methods here
+
+  Future<void> getContents() async {
+    try {
+      emit(const DailyLoading());
+
+      List<Art> contents = <Art>[];
+
+      //query and get items from db
+
+      for (var i = 0; i < 3; i++) {
+        await Db().query(Que.values[i].getQue).then(
+          (value) {
+            if (value != null) {
+              for (var element in value) {
+                contents.add(
+                  Art.fromList(element),
+                );
+              }
+            }
+          },
+        );
+      }
+
+      emit(DailyLoaded(contents));
+    } catch (e) {
+      emit(DailyError(e.toString()));
+    }
+  }
+}
---
+++ lib/screens/home/states_daily_content.dart
@@ -0,0 +1,23 @@
+import 'package:art_exhibition/db/models/art/model_art.dart';
+
+abstract class DailyContentStates {
+  const DailyContentStates();
+}
+
+class DailyInitial extends DailyContentStates {
+  const DailyInitial();
+}
+
+class DailyLoading extends DailyContentStates {
+  const DailyLoading();
+}
+
+class DailyLoaded extends DailyContentStates {
+  final List<Art> contents;
+  const DailyLoaded(this.contents);
+}
+
+class DailyError extends DailyContentStates {
+  final String message;
+  const DailyError(this.message);
+}
