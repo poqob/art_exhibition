b9385d731a808b1bbe558feaa9e2de7c243911da
Mustafa <mmermmus123@gmail.com>
2023-03-14 18:08:22 +0300

    tests

--- test/test.dart
+++ test/test.dart
@@ -1,8 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:art_exhibition/utilities/save_content/content.dart';
-import 'package:art_exhibition/utilities/save_content/saved.dart';
+import 'package:art_exhibition/data/saved_data/content.dart';
 
 void main() async {
   File file = File('D:\\Dosyalar\\Flutter\\art_exhibition\\test\\test.json');
@@ -10,15 +9,16 @@ void main() async {
       "heading", List.generate(2, (index) => index), "imgPath", "contentText");
   Content a = Content(
       "baslik", List.generate(2, (index) => index), "imgPath", "contentText");
-  Saved.instance.add(c);
-  Saved.instance.add(a);
-  print(Saved.instance.contents.length);
+  //Saved.instance.contents.clear();
+  //Saved.instance.add(c);
+  //Saved.instance.add(a);
+  //print(Saved.instance.contents.length);
   //Saved.getInstance.remove(c);
-  print(Saved.instance.contents.length);
-  print(Saved.instance.contents.first.heading);
-  Saved.instance.contents.map((e) => e.toJson()).toList();
-  file.writeAsString(jsonEncode(Saved.instance.contents),
-      mode: FileMode.writeOnly);
+  //print(Saved.instance.contents.length);
+  //print(Saved.instance.contents.first.heading);
+  // Saved.instance.contents.map((e) => e.toJson()).toList();
+  //file.writeAsString(jsonEncode(Saved.instance.contents),
+  //   mode: FileMode.writeOnly);
 
   List readedDate = [];
   await file.readAsString().then((value) {
