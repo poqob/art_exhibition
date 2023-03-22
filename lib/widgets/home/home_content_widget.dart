dd168686bdded9c2dacd9f90e29e49a92f247370
Mustafa <mmermmus123@gmail.com>
2023-03-22 22:07:51 +0300

    appbar added

--- lib/widgets/home/home_content_widget.dart
+++ lib/widgets/home/home_content_widget.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: invalid_use_of_protected_member
 
+import 'package:art_exhibition/constants/constant_backround_color.dart';
+import 'package:art_exhibition/constants/constant_colors.dart';
 import 'package:art_exhibition/screens/content_page/single_content_page.dart';
 import 'package:art_exhibition/utilities/extension_layout.dart';
 import 'package:art_exhibition/utilities/todo.dart';
@@ -13,17 +15,26 @@ Padding homeContentWidget(BuildContext context, State state, dynamic widget) {
       child: ElevatedButton(
         onPressed: () {
           state.setState(() {
-            widget.current = widget.contents[3];
+            widget.current = widget.contents[3]; //i'm going to change it.
           });
           showDialog(
-            context: context,
-            builder: (context) => SingleContentPage(
-              id: [widget.current!.artName, widget.current!.artId],
-              heading: widget.current!.artName,
-              imagePath: widget.current!.imgPath,
-              text: widget.current!.content,
-            ),
-          );
+              context: context,
+              builder: (context) => Scaffold(
+                    appBar: AppBar(
+                      backgroundColor:
+                          ConstantBackroundColors.colorContentPage.getColor,
+                      leading: IconButton(
+                        onPressed: () => Navigator.pop(context),
+                        icon: const Icon(Icons.arrow_back, color: Colors.white),
+                      ),
+                    ),
+                    body: SingleContentPage(
+                      id: [widget.current!.artName, widget.current!.artId],
+                      heading: widget.current!.artName,
+                      imagePath: widget.current!.imgPath,
+                      text: widget.current!.content,
+                    ),
+                  ));
         },
         style: ButtonStyle(
           overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
