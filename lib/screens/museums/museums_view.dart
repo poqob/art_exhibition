8215a3f0f6475dbbcb540cf678507c452e401c73
Mustafa <mmermmus123@gmail.com>
2023-03-22 22:08:26 +0300

    content pages created-testable

--- lib/screens/museums/museums_view.dart
+++ lib/screens/museums/museums_view.dart
@@ -1,9 +1,11 @@
+import 'package:art_exhibition/data/saved_data/content.dart';
 import 'package:art_exhibition/db/bloc/museum/bloc_museum.dart';
 import 'package:art_exhibition/db/bloc/museum/states_museum.dart';
+import 'package:art_exhibition/screens/content_page/single_content_page.dart';
+import 'package:art_exhibition/utilities/todo.dart';
 import 'package:art_exhibition/widgets/common/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:logger/logger.dart';
 
 class MuseumView extends StatelessWidget {
   const MuseumView({super.key});
@@ -25,9 +27,7 @@ class Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<MuseumCubits, MuseumStates>(
-      listener: (BuildContext context, Object? state) {
-        Logger().d(state);
-      },
+      listener: (BuildContext context, Object? state) {},
       builder: (BuildContext context, state) {
         if (state is MuseumInitial) {
           context.read<MuseumCubits>().getMuseums();
@@ -36,7 +36,20 @@ class Page extends StatelessWidget {
           return const Loading();
         } else if (state is MuseumLoaded) {
           //convert readed data to list of Content() then send them to Body.
-          return const Body();
+
+          List<Content> list = <Content>[];
+          for (var element in state.museums) {
+            list.add(Content(
+              element.museumName!,
+              [element.museumName!, element.museumId!],
+              element.imagePath!,
+              element.content!,
+            ));
+          }
+
+          return Body(
+            list: list,
+          );
         } else /*MuseumError situation*/ {
           return const ErrorWidget();
         }
@@ -65,20 +78,82 @@ class ErrorWidget extends StatelessWidget {
   }
 }
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
+  final List<Content> list;
   const Body({
+    required this.list,
     super.key,
   });
 
   @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> with SingleTickerProviderStateMixin {
+  late PageController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = PageController();
+  }
+
+  @Todo("these debug buttons writed to provide tests on windows.")
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text(
-          "Museum",
-          style: Theme.of(context).textTheme.displaySmall,
-        ),
+      body: PageView(
+        controller: _controller,
+        pageSnapping: true,
+        allowImplicitScrolling: true,
+        scrollDirection: Axis.horizontal,
+        children: List.generate(widget.list.length, (index) {
+          Content content = widget.list[index];
+          return Scaffold(
+            body: Stack(
+              children: [
+                SingleContentPage(
+                    heading: content.heading,
+                    imagePath: content.imgPath,
+                    text: content.contentText,
+                    id: content.id),
+                Positioned(
+                  left: 5,
+                  bottom: 10,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _controller.nextPage(
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.linear);
+                    },
+                    child: const Text("debug next"),
+                  ),
+                ),
+                Positioned(
+                  left: 5,
+                  bottom: 45,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _controller.previousPage(
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.linear);
+                    },
+                    child: const Text("debug prev"),
+                  ),
+                ),
+                Positioned(
+                  left: 5,
+                  bottom: 80,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text("debug back"),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }),
       ),
     );
   }
