767f223acdbf8eb2e59a188cca7b1b140ed70a73
Mustafa <mmermmus123@gmail.com>
2023-04-02 00:35:58 +0300

    new postresql functions added

--- lib/db/api/queries.dart
+++ lib/db/api/queries.dart
@@ -20,6 +20,14 @@ mixin mi {
         return "select * from artists.allArtists();";
       case "allMuseums":
         return "select * from museums.allMuseums();";
+      case "allPaints":
+        return "select * from arts.allpaints();";
+      case "allStatues":
+        return "select * from arts.allstatues();";
+      case "allDecorative":
+        return "select * from arts.alldecorative();";
+      case "allArchitecture":
+        return "select * from arts.allarchitecture();";
       default:
         return "$artQueryRoot arts.$name();";
     }
