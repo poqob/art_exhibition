fff1366ac4834385a93368be99b3eb9814604800
Mustafa <mmermmus123@gmail.com>
2023-04-01 22:24:04 +0300

    account settings option added

--- lib/screens/settings/settings_page.dart
+++ lib/screens/settings/settings_page.dart
@@ -1,3 +1,5 @@
+import 'package:art_exhibition/db/api/authentication.dart';
+import 'package:art_exhibition/utilities/extension_layout.dart';
 import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -11,11 +13,32 @@ class SettingsScreen extends StatelessWidget {
         onPressed: () => Navigator.pop(context),
         icon: const Icon(Icons.arrow_back, color: Colors.black),
       )),
-      body: Center(
-        child: Text(
-          "Settings",
-          style: Theme.of(context).textTheme.displaySmall,
-        ),
+      body: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: context.dynamicWidth(0.05),
+                vertical: context.dynamicWidth(0.05)),
+            child: ListTile(
+              title: Text(
+                "Account Setting",
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              trailing:
+                  Text("logged as ${Authentication.instance.getUsername}"),
+              subtitle: const Text("account info..."),
+              hoverColor: Colors.white60,
+              leading: const Icon(Icons.person),
+              onTap: () {},
+            ),
+          ),
+          Center(
+            child: Text(
+              "Settings",
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+          ),
+        ],
       ),
     );
   }
