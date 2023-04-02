16571289af4ba24931d0d043fe644b87640f1dba
Mustafa <mmermmus123@gmail.com>
2023-04-02 03:19:53 +0300

    working on grid view of arts and catagories

--- lib/screens/arts/grid_view/grid_view.dart
+++ lib/screens/arts/grid_view/grid_view.dart
@@ -12,17 +12,117 @@ import 'package:art_exhibition/widgets/common/loading_error_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ArtsPageView extends StatelessWidget {
+@Todo('''
+
+chipsler dinamik oldu değerleri ve kendileri degisebiliyor ancak,
+blok tuketici icini set state ile yenileyemiyorum????
+
+kod temizlenecek!
+
+
+''')
+
+//keeps current art catagory content list to provide to appbar.
+class _CurrentContent {
+  static List<Art> list = <Art>[];
+  static String tittle = "";
+  static Que que = Que.allPaints;
+}
+
+class ArtsPageView extends StatefulWidget {
   const ArtsPageView({super.key});
 
+  @override
+  State<ArtsPageView> createState() => _ArtsPageViewState();
+}
+
+class _ArtsPageViewState extends State<ArtsPageView> {
   @Todo("que parameter will comes from out of class")
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appbar(context),
+      backgroundColor: ConstantBackroundColors.colorContentPage.getColor,
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 5,
+              child: _catagories(this),
+            ),
+            Expanded(
+              flex: 60,
+              child: _body(this),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _catagories(State state) {
+    List<Widget> categories = [
+      _category(tittle: "paints", que: Que.allPaints, state: state),
+      _category(tittle: "decoratives", que: Que.allDecorative, state: state),
+      _category(
+          tittle: "architectures", que: Que.allArchitecture, state: state),
+      _category(tittle: "statues", que: Que.allStatues, state: state),
+    ];
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: categories,
+    );
+  }
+
+  Widget _category(
+      {required String tittle, required Que que, required State state}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5),
+      child: GestureDetector(
+        onTap: () {
+          state.setState(() {
+            _CurrentContent.tittle = tittle;
+            _CurrentContent.que = que;
+          });
+        },
+        child: Chip(
+          avatar: tittle == _CurrentContent.tittle
+              ? const Icon(Icons.done, color: Colors.white)
+              : null,
+          label: Text(tittle),
+          labelStyle: const TextStyle(color: Colors.white),
+          backgroundColor: ConstantBackroundColors.colorContentPage.getColor,
+          side: const BorderSide(
+              color: Colors.white, width: 1, style: BorderStyle.solid),
+        ),
+      ),
+    );
+  }
+
+  BlocProvider<ArtCubit> _body(State state) {
     return BlocProvider(
       create: (BuildContext context) => ArtCubit(),
-      child: const Page(
-        que: Que.allPaints,
+      child: Page(
+        que: _CurrentContent.que,
       ),
+    );
+  }
+
+  AppBar _appbar(BuildContext context) {
+    return AppBar(
+      actions: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: IconButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) =>
+                        ArtPageView(list: artToContent(_CurrentContent.list)));
+              },
+              icon: const Icon(Icons.change_circle_outlined)),
+        )
+      ],
     );
   }
 }
@@ -45,59 +145,12 @@ class Page extends StatelessWidget {
         } else if (state is ArtLoading) {
           return const Loading();
         } else if (state is ArtLoaded) {
-          return Body(
-            list: state.arts,
-          );
+          _CurrentContent.list = state.arts;
+          return gridview(artToContent(state.arts));
         } else {
           return const LoadingErrorWidget();
         }
       },
-    );
-  }
-}
-
-class Body extends StatefulWidget {
-  final List<Art> list;
-  const Body({
-    required this.list,
-    super.key,
-  });
-
-  @override
-  State<Body> createState() => _BodyState();
-}
-
-class _BodyState extends State<Body> with SingleTickerProviderStateMixin {
-  Widget? bodyC;
-
-  @override
-  void initState() {
-    super.initState();
-    bodyC = gridview(artToContent(widget.list));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: IconButton(
-                onPressed: () {
-                  setState(() {
-                    showDialog(
-                        context: context,
-                        builder: (context) =>
-                            ArtPageView(list: artToContent(widget.list)));
-                  });
-                },
-                icon: const Icon(Icons.change_circle_outlined)),
-          )
-        ],
-      ),
-      body: bodyC,
-      backgroundColor: ConstantBackroundColors.colorContentPage.getColor,
     );
   }
 }
