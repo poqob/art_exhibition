bb53e60081dc1c3bc8f112addbd5ad6e5626401e
Mustafa <mmermmus123@gmail.com>
2023-04-29 00:23:00 +0300

    logic methods will wrote here

---
+++ lib/screens/settings/logic/accountSettingsMixin.dart
@@ -0,0 +1,7 @@
+import 'package:art_exhibition/utilities/todo.dart';
+
+@Todo("fill methods, data will come here filter aplied.")
+mixin AccountSettingsMixin {
+  Future<void> changePassword(String newPass) async {}
+  Future<void> changeUsername(String newName) async {}
+}
